082ef8d7d2be3b0a4d7a3eafd773918540d40e2d
Andrew Sather <andrewjsather@gmail.com>
2021-01-03 18:31:48 -0500

    Excluding items older than 30 days

--- add_sources.py
+++ add_sources.py
@@ -1,6 +1,8 @@
 from pocky import pocky
 from pathlib import Path
 import feedparser
+import datetime
+from datetime import timedelta
 
 key_path = Path('C:/Users/Andrew/OneDrive/Documents/Code Keys/Pocket/keys.json')
 p = pocky(key_path)
@@ -13,12 +15,22 @@ for f in p.get_sources().keys():
     feed_url = source['url']
     tags = source['tags']
     feed = feedparser.parse(feed_url)
-    print(feed)
+    #print(feed)
 
+    #Don't import articles more than 30 days old.
+    oldest_article = datetime.datetime.now() - timedelta(days=30)
+
+    #Loop through every tiem...
     for i in feed['items']:
         #Don't add if it's already been added.
         if(i['link'] in history):
             print('Already added. Skipping: ', i['title'], i['link'])
+            continue
+
+        #Don't import articles that are too old.
+        published_date = datetime.datetime(i['published_parsed'][0], i['published_parsed'][1], i['published_parsed'][2])
+        if(published_date < oldest_article):
+            print('Skipping: Article is too old to add.')
             continue
 
         print('ADDING.....')
